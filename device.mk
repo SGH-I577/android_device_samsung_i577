fe14c22a10d1aaf53aac58f0867d3ef1f2b8b6ff
bigdaddy92474@gmail.com <ktemp@buildmachine2.(none)>
2013-06-21 02:02:42 -0500

    Bring up to cm-10.1

--- device.mk
+++ device.mk
@@ -14,11 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-## The gps config appropriate for this device
-PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
-
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/i577/i577-vendor.mk)
 
@@ -29,42 +24,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/i577/overlay
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-# Hardware
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
-
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    device/samsung/i577/ramdisk/init.qcom.rc:root/init.qcom.rc \
-    device/samsung/i577/ramdisk/init.qcom.sh:root/init.qcom.sh \
     device/samsung/i577/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
-    device/samsung/i577/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
-    device/samsung/i577/ramdisk/init.target.rc:root/init.target.rc \
-    device/samsung/i577/ramdisk/ueventd.rc:root/ueventd.rc \
-    device/samsung/i577/ramdisk/init.emmc.rc:root/init.emmc.rc \
-    device/samsung/i577/fstab.qcom:root/fstab.qcom
-
+    
 # BT firmware
 PRODUCT_COPY_FILES += \
     device/samsung/i577/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
 
-# Vold
-PRODUCT_COPY_FILES += \
-    device/samsung/i577/vold.fstab:system/etc/vold.fstab
-
-# QRNGD
-PRODUCT_PACKAGES += qrngd
-
-# common msm8660
-$(call inherit-product, device/samsung/msm8660-common/msm8660.mk)
-
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/i577/i577-vendor.mk)
+# Inherit from celox-common
+$(call inherit-product, device/samsung/celox-common/celox-common.mk) 
 
 # NFC
 BOARD_HAVE_NFC := true
